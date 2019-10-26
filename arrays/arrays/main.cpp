2a601d2f84a0517d19373d4dd6d98950b3e7a608
himanchalsingh93@gmail.com <himanchalsingh93@gmail.com>
2019-10-26 14:27:43 +0530

    added comment

--- arrays/arrays/main.cpp
+++ arrays/arrays/main.cpp
@@ -2,15 +2,20 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    int example[5];
-    
+    int example[5]; //stored on stack
     for (int i = 0; i < 5; i++) {
         example[i] = 23;
     }
     
+    int* another = new int[5]; //created on heap
+    for (int i = 0; i < 5; i++) {
+        another[i] = 23;
+    }
+    delete [] another;
+    
     int* ptr = example; //it is just an integer pointer in the memory
     *(ptr + 2) = 5; //same as above line
     
-    
+    //change
     return 0;
 }
